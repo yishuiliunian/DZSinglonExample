fec5f0c62242ee3c8f86fdac830efd2a15e011b4
Stone Dong <stonedong@tencent.com>
2014-01-17 16:36:06 +0800

    Initial Commit

---
+++ DZSinglonExample/DZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DZAppDelegate.h
+//  DZSinglonExample
+//
+//  Created by Stone Dong on 14-1-17.
+//  Copyright (c) 2014年 Stone Dong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
